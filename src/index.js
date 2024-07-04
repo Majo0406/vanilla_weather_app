ed9aa14aa4c48ef54d90cac1d892c1766b4b70a4
Maria Jose Reyes <moren741@gmail.com>
2024-07-04 13:10:11 +0100

    Added min and max temp data

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ function searchCity (city) {
 function weatherResponse(response){
     let temperatureElement = document.querySelector("#current-tempeture");
     let tempeture = Math.round(response.data.temperature.current);
+    let mintempElement = document.querySelector(".mintemp");
+    let maxtempElement = document.querySelector(".maxtemp");
+    let mintemp = Math.round(response.data.temperature.minimum);
+    let maxtemp = Math.round(response.data.temperature.maximum);
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#current-description");
     let humidityElement = document.querySelector("#humidity");
@@ -19,6 +23,8 @@ function weatherResponse(response){
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
     temperatureElement.innerHTML = tempeture;
+    mintempElement.innerHTML = mintemp;
+    maxtemp.innerHTML = maxtemp;
     descriptionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = response.data.temperature.humidity;
     windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
